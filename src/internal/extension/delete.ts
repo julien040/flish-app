46a6c55066d37a2bd9c7b7f9942cfd3fd6012e14
Julien <48369040+julien040@users.noreply.github.com>
2022-04-02 19:01:48 +1100

    🧹 Extensions and instances deletion API implemented

--- src/internal/extension/delete.ts
+++ src/internal/extension/delete.ts
@@ -1,3 +1,39 @@
-/* 
-Before writing this function, we need to be sure than no instance use the extension. This API should be written later
-*/
+import { getAllInstances } from "../instance/read";
+import { deleteConfig } from "../store";
+import getAppDataPath from "appdata-path";
+import { join } from "path";
+import { rmdir } from "fs/promises";
+import captureEvent from "../analytics";
+
+const userPath = getAppDataPath("flish");
+
+/** An async function that delete an extension
+ *
+ * First, it checks if the extension is used by any instance. If it is, it throws an error and stop the process.
+ * If not, it deletes the extension from the db and wipe the extension folder.
+ * @param  {string} id The id of the extension to delete
+ * @returns Promise
+ */
+async function deleteExtension(id: string): Promise<void> {
+  const instances = await getAllInstances();
+  // Check if there are any instances using this extension
+  for (const key in instances) {
+    if (Object.prototype.hasOwnProperty.call(instances, key)) {
+      const element = instances[key];
+      if (element.extensionID === id) {
+        throw new Error(`Extension ${id} is used by instance ${element.name}`);
+      }
+    }
+  }
+  // Delete folder
+  const folderToDelete = join(userPath, "extensions", id);
+  await rmdir(folderToDelete, { recursive: true });
+
+  // Delete config
+  await deleteConfig(`extensions.${id}`);
+  captureEvent("Extension deleted", {
+    extensionID: id,
+  });
+}
+
+export default deleteExtension;
--- src/internal/instance/delete.ts
+++ src/internal/instance/delete.ts
@@ -1,9 +1,42 @@
-/* import { deletePassword } from "keytar";
+import { deletePassword } from "keytar";
 import { deleteConfig, getConfig } from "../store";
 import config from "../../config";
 import { getExtension } from "../extension/read";
+import { Instance } from "./types";
+import captureEvent from "../analytics";
+import { session } from "electron";
 
-export const deleleInstance = async (id: string) => {
-  //To do later
-};
+/** An async function that delete an instance
+ *
+ * It removes the instance from the db, delete all env variables and delete all session data
+ * @param  {string} id The id of the instance to delete
+ * @returns Promise
  */
+export const deleteInstance = async (id: string): Promise<void> => {
+  const instance: Instance = await getConfig(`instances.${id}`);
+  if (!instance) {
+    throw new Error(`Instance ${id} does not exist`);
+  }
+  const extension = await getExtension(instance.extensionID);
+  // Delete env variables
+  for (let index = 0; index < extension.envVariables.length; index++) {
+    const element = extension.envVariables[index];
+    if (element.needToBeEncrypted === true) {
+      await deletePassword(
+        config.secureStoreAppName,
+        `${instance.instanceID}-${element.name}`
+      );
+    } else {
+      await deleteConfig(`envVariable.${instance.instanceID}.${element.name}`);
+    }
+  }
+  const instanceSession = session.fromPartition(
+    "persist:" + instance.instanceID
+  );
+  await instanceSession.clearStorageData();
+  await deleteConfig(`instances.${id}`);
+  captureEvent("Instance deleted", {
+    extensionID: instance.extensionID,
+    name: instance.name,
+  });
+};
